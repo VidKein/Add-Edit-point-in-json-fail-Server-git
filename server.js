a5c4a673e65da59fd11fdac48857553c47cc995b
VidKein <landdru@ukr.net>
2024-12-26 14:35:04 +0100

    +

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 4000; // Используется переменная окружения или 4000 по умолчанию
 // Абсолютный путь к файлу
 const path = require('path');
-const DATA_FILE = path.resolve(__dirname, '/data/koordinats.json');
+const DATA_FILE = path.resolve(__dirname, './data/koordinats.json');
 //
 app.use(express.json());
 app.use(cors()); // Разрешаем CORS для всех источников
